0b2f29b4e1e63725bbb279f8bf0c1ca67095540b
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-01-09 20:52:52 +0300

    create solidity code in mnarketplace contract and ready template for marketplace.sol

---
+++ solidity/contracts/Marketplace.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Marketplace{
+    address public owner;
+
+    function startNftSale() public{
+
+    }
+
+    function cancelNftSale() public{
+
+    }
+
+    function startNftAuction() public {
+
+    }
+
+    function cancelNftAuction() public{
+
+    }
+
+    function buyNft() public{
+
+    }
+
+    function bid() public payable{
+
+    }
+
+    function finishNftAuction() public {
+
+    }
+}
