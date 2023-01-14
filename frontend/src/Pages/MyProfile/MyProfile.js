ab65dbabec48244039970c8abb9fb1f3824800de
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-01-14 22:56:02 +0300

    added last changes

--- frontend/src/Pages/MyProfile/MyProfile.js
+++ frontend/src/Pages/MyProfile/MyProfile.js
@@ -2,10 +2,12 @@ import React, { useEffect } from "react";
 import styles from "./MyPofile.module.scss";
 import { useGetNFTs } from "../../Hooks/useGetNFTs";
 import { useSelector } from "react-redux";
+import { NFT } from "../../Components";
 
 export const MyProfile = () => {
   const { getNFTs } = useGetNFTs();
   const account = useSelector((state) => state.accounts.account);
+  const nft = useSelector((state) => state.accounts.myNFTs);
   useEffect(() => {
     const work = async () => {
       await getNFTs();
@@ -13,8 +15,13 @@ export const MyProfile = () => {
     work();
   }, [account]);
   return (
-    <div>
-      <h1>My Profile</h1>
+    <div className={styles.wrapper}>
+      {
+        nft?.map((nft) => {
+          return <NFT key={nft.tokenId} nft={nft} />
+        }
+        )
+      }
     </div>
   );
 };
