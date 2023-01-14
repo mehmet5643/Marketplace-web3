3efb91e12433c4d6c824be7f5102576b0c93b621
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-01-14 21:55:36 +0300

    added request with alchemy api getNfts

--- frontend/src/Pages/MyProfile/MyProfile.js
+++ frontend/src/Pages/MyProfile/MyProfile.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import styles from "./MyPofile.module.scss"
+import React, { useEffect } from "react";
+import styles from "./MyPofile.module.scss";
+import { useGetNFTs } from "../../Hooks/useGetNFTs";
+import { useSelector } from "react-redux";
 
 export const MyProfile = () => {
+  const { getNFTs } = useGetNFTs();
+  const account = useSelector((state) => state.accounts.account);
+  useEffect(() => {
+    const work = async () => {
+      await getNFTs();
+    };
+    work();
+  }, [account]);
   return (
     <div>
-      <h1>
-        My Profile  
-      </h1>
+      <h1>My Profile</h1>
     </div>
-  )
-}
+  );
+};
